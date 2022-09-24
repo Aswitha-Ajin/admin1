61a225f38b60e19444918d905b34da5f773e3b22
Aswitha-Ajin <1112aswitha@gmail.com>
2022-09-24 21:55:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
+import Buttons from "./Components/Button";
+import Cards from "./Components/Cards";
+import Tables from "./Components/Table";
+
+import "./css/sb-admin-2.min.css";
+
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/button" element={<Buttons />} />
+          <Route path="/card" element={<Cards />} />
+          <Route path="/table" element={<Tables />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
